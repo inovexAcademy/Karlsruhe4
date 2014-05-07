332218bbb39ec3cc9d15213b46c64c6794f4265e
Markus Feindler <markus.feindler@inovex.de>
2014-05-07 15:22:10 +0200

    no message

--- src/main/java/de/inovex/academy/task2/TemplateEngine.java
+++ src/main/java/de/inovex/academy/task2/TemplateEngine.java
@@ -11,12 +11,21 @@ public class TemplateEngine {
 		this.template = template;
 	}
 	
-	public String render(Map<String, String> vars) {
+	public String render(Map<String, String> vars) throws PlaceHolderException {
 		for (String var : vars.keySet()) {
+			
+			if (!containsPlaceHolder(var)) {
+				throw new PlaceHolderException();
+			}
+			
 			template = template.replace("${" + var + "}", vars.get(var));
 		}
 		
 		return template;
+	}
+	
+	public boolean containsPlaceHolder(String placeholder) {
+		return template.contains("${" + placeholder + "}");
 	}
 
 }
--- src/test/java/de/inovex/academy/task2/TemplateEngineTest.java
+++ src/test/java/de/inovex/academy/task2/TemplateEngineTest.java
@@ -2,6 +2,7 @@ package de.inovex.academy.task2;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,6 +21,37 @@ public class TemplateEngineTest {
 		assertThat(getTemplateEngine("Hallo").render(new HashMap<String, String>()), is("Hallo"));
 		
 		assertThat(getTemplateEngine("Hallo ${name}").render(Collections.singletonMap("name", "Klaus")), is("Hallo Klaus"));
-
+		
+		Map<String, String> vars = new HashMap<String, String>();
+		
+		vars.put("vorname", "Klaus");
+		vars.put("nachname", "Meyer");
+		
+		assertThat(getTemplateEngine("Hallo ${vorname} ${nachname}").render(vars), is("Hallo Klaus Meyer"));
 	}
+	
+	@Test
+	public void testContainsPlaceHolder() {
+		String template = "Hallo ${vorname} ${nachname}";
+		
+		Map<String, String> vars = new HashMap<String, String>();
+		
+		vars.put("vorname", "Klaus");
+		vars.put("nachname", "Meyer");
+		vars.put("geschlecht", "nix");
+		
+		assertThat(getTemplateEngine("Hallo ${vorname} ${nachname}").containsPlaceHolder("geschlecht"), is(false));
+	}
+	
+	@Test(expected = PlaceHolderException.class)
+	public void testPlaceHolderExceptionIsThrownWhenPlaceholderNotFound() {
+		Map<String, String> vars = new HashMap<String, String>();
+
+		vars.put("vorname", "Klaus");
+		vars.put("nachname", "Meyer");
+		vars.put("geschlecht", "nix");
+		
+		getTemplateEngine("Hallo ${vorname} ${nachname}").render(vars);
+	}
+	
 }
